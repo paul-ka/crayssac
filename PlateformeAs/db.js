f64c443e69f59d8ae3a77184e3efa48d63bf7691
Crayssac <paul-ka@laposte.net>
2025-08-10 22:27:03 +0200

    Add the json of Plateforme AS card

---
+++ PlateformeAs/db.js
@@ -0,0 +1,161 @@
+var bdd = {
+	"eleve" : "",
+	"patients" : [
+		{
+		"civilite":"M.",
+		"nom":"DUMAS",
+		"prenom":"Alexandre",
+		"adresse":"Av. Dartagnan",
+		"codepost":"75000",
+		"ville":"Paris",
+		"tel":"0102030405",
+		"contact":"EugÃ©nie DUMAS (fille)",
+		"medecin":"Dr Toubib",
+		"mac_maladie":"macrocible maladie",
+		"mac_trait":"macrocible traitement",
+		"mac_vecu":"macrocible vecu",
+		"mac_env":"macrocible environement",
+		"mac_dev":"macrocible devenir",
+		"soin":[
+			{"date":"1/1/2000",
+			"heure":"8h",
+			"realise":"oui",
+			"soin":"2",
+			"obs":"Blabla sur observation"
+			},
+			{"date":"2/1/2000",
+			"heure":"10h",
+			"realise":"oui",
+			"soin":"4",
+			"obs":"Autre Blabla sur observation"
+			}
+		],
+		"dg":[
+			{"heure":"6",
+			"fr":"20",
+			"urine":"500",
+			"tc":"60",
+			"ta":"8",
+			"t":"37"
+			},
+			{"heure":"7",
+			"fr":"35",
+			"urine":"800",
+			"t":"37.8"
+			},
+			{"heure":"8",
+			"fr":"25",
+			"urine":"600",
+			"tc":"70",
+			"ta":"13",
+			"t":"37.4"
+			},
+			{"heure":"9",
+			"fr":"35"
+			},
+			{"heure":"10",
+			"fr":"25",
+			"urine":"600",
+			"tc":"70",
+			"ta":"13",
+			"t":"37.4"
+			},
+			{"heure":"11",
+			"fr":"25",
+			"urine":"650",
+			"tc":"65",
+			"ta":"13",
+			"t":"37.2"
+			}
+		],
+		"trans":[
+			{"date":"Date",
+			"cible":"Cible",
+			"données":"Données",
+			"action":"Action",
+			"résultat":"Résutat",
+			},
+			{"date":"1",
+			"cible":"2",
+			"données":"3",
+			"action":"4",
+			"résultat":"5",
+			}
+		]
+		},
+		{"civilite":"M.",
+		"nom":"HUGO",
+		"prenom":"Victor",
+		"adresse":"Rue des misérables",
+		"codepost":"75100",
+		"ville":"Paris",
+		"tel":"0101010101",
+		"contact":"Anna HUGO (fille)",
+		"medecin":"Dr Knock"
+		},
+		{"civilite":"Mme.",
+		"nom":"SAND",
+		"nom_nais":"CHAIPA",
+		"prenom":"Georges",
+		"adresse":"Rue des fadettes",
+		"codepost":"36120",
+		"ville":"La Chatres",
+		"tel":"0201010101",
+		"contact":"Marie SAND (fille)",
+		"medecin":"Dr Pamal",
+		"soin":[
+			{"date":"1/1/1789",
+			"heure":"12h",
+			"realise":"",
+			"soin":"10",
+			"obs":"Blabla"
+			},
+			{"date":"2/1/1789",
+			"heure":"8h",
+			"realise":"",
+			"soin":"8",
+			"obs":"Autre Blabla"
+			}
+		]
+		},
+		{"civilite":"Mme.",
+		"nom":"DURAS",
+		"prenom":"Marguerite"
+		},
+		{"civilite":"M",
+		"nom":"ATTILA",
+		"nom_nais":"",
+		"prenom":"Huns",
+		"adresse":"Rue de la toundra",
+		"codepost":"111111",
+		"ville":"",
+		"tel":"1111111111",
+		"contact":"",
+		"medecin":"",
+		"dg":[
+			{"heure":"11",
+			"fr":"20",
+			"urine":"500",
+			"tc":"60",
+			"ta":"8",
+			"t":"37"
+			}
+		],
+		"soin":[
+			{"date":"1/1/1111",
+			"heure":"11h11",
+			"realise":"oui",
+			"soin":"14",
+			"obs":"du bout du nez jusqu'aux orteilles"
+			},
+			{"date":"11/11/1111",
+			"heure":"10h",
+			"realise":"oui",
+			"soin":"4",
+			"obs":"Blabla sur observation"
+			}
+		]
+		}
+	]
+}
+;
